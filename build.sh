6712f276e60344c7f7a25746cf5daffa7d6dd090
lz <pro@liuzhendeMacBook-Pro.local>
2019-07-11 16:19:33 +0800

    feat: add bash

--- build.sh
+++ build.sh
@@ -6,9 +6,10 @@ if [ $1 ]
 then
     echo -e "\033[36m tagId = $1 \033[0m" 
     cd $projectdir
+    git checkout test
+    git pull --all
+    
     if git rev-parse -q --verify "refs/tags/$1" >/dev/null; then
-        git checkout test
-        git pull --all
         git checkout $1
         yarn install
         yarn run build:doc
